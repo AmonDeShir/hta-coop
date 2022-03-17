262893b653bc742b631af099e67521e2fd1851f3
Amon De Shir <amonsir29@gmail.com>
2022-03-17 03:37:17 +0100

    Remove unused shaders and functions

--- client/scripts/camera.gd
+++ client/scripts/camera.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 export(NodePath) var path_to_target: NodePath
-export var follow_acceleration := 0.25
+export var follow_acceleration := 0.15
 export var vertical_min := -55.0
 export var vertical_max := 75.0
 export var horizontal_sensitivity := 0.1
@@ -14,6 +14,7 @@ var camera_rotation_v := 0.0
 var target: Spatial
 var current_position := Vector3(0, 0, 0)
 var ideal_offset := Vector3(0, 0, 0)
+
 
 func _ready():
 	set_physics_process(true);
@@ -29,48 +30,23 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		camera_rotation_h += -event.relative.x * horizontal_sensitivity
 		camera_rotation_v -= event.relative.y * vertical_sensitivity
-		$"mouse-control-stay-delay".start()
 
 
 func _physics_process(delta):
 	_follow_target(delta)
-	
-	if $"mouse-control-stay-delay".is_stopped():
-		_apply_rotation_camera(delta)
-	else:
-		_apply_mouse_rotation(delta)
+	_apply_mouse_rotation(delta)
 
 
 func _follow_target(delta: float):
 	var acceleration = _make_fps_independent(follow_acceleration, delta)
-	var ideal_offset := _calculate_ideal_offset()
+	var offset := ideal_offset + target.translation
 
-	current_position = current_position.linear_interpolate(ideal_offset, acceleration)
+	current_position = current_position.linear_interpolate(offset, acceleration)
 	self.translation = 	current_position
 
 
 func _make_fps_independent(variable: float, delta_time: float):
 	return variable * pow(0.001, delta_time)
-
-
-func _calculate_ideal_offset() -> Vector3:
-	#var offset = _apply_quaternion(ideal_offset, target.transform.basis.get_rotation_quat())
-	return ideal_offset + target.translation
-
-
-func _apply_quaternion(v: Vector3, q: Quat) -> Vector3:
-	var u := Vector3(q.x, q.y, q.z)
-	var s := q.w
-
-	return 2.0 * u.dot(v) * u + (s*s - u.dot(u)) * v + 2.0 * s * u.cross(v)
-
-
-func _apply_rotation_camera(delta: float):
-	var target_rotation := target.global_transform.basis.get_euler().y
-	var acceleration = _make_fps_independent(follow_acceleration, delta)
-	
-	$"h".rotation.y = lerp_angle($"h".rotation.y, target_rotation, acceleration)
-	camera_rotation_h = $"h".rotation.y
 
 
 func _apply_mouse_rotation(delta: float):
